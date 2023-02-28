fec156f78e998c79717d45cee11febe82bb663b5
‘th2205’ <th05662205@gmail.com>
2023-02-28 13:24:19 +0900

    update

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip test")
+    public void vip_0 () {
+//        Member member = new Member();
+    }
+}
